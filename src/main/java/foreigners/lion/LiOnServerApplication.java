8e39f5a88f2e89f33d79cb8d922f55286ac79966
tlgms <siheun9@gmail.com>
2026-07-12 18:56:37 +0900

    feat: 기본 Spring Boot 프로젝트 설정

---
+++ src/main/java/foreigners/lion/LiOnServerApplication.java
@@ -0,0 +1,13 @@
+package foreigners.lion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiOnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiOnServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/foreigners/lion/LiOnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package foreigners.lion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiOnServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
