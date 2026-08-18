7fe6d314a9f9821c9479fd85831979ba6ee4cc34
tlgms <siheun9@gmail.com>
2026-08-18 16:58:45 +0900

    fix: 예외처리 구문 삭제(#8)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,10 +4,6 @@ import { PrismaClient } from '../../generated/prisma/client.js';
 
 const databaseUrl = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL 환경 변수가 설정되지 않았습니다.');
-}
-
 @Injectable()
 export class PrismaService
   extends PrismaClient
