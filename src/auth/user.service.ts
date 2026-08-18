95cfdda2da61e0d7633035cae73c56039d5591f0
tlgms <siheun9@gmail.com>
2026-08-18 20:16:14 +0900

    feat: UserService 구현(#8)

---
+++ src/auth/user.service.ts
@@ -0,0 +1,31 @@
+import { Prisma, User } from "generated/prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUserById(userId: number): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { id: userId },
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({
+      data,
+    });
+  }
+
+  async updateUser(userId: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prismaService.user.update({
+      where: { id: userId },
+      data,
+    });
+  }
+
+  async deleteUser(userId: number): Promise<User> {
+    return this.prismaService.user.delete({
+      where: { id: userId },
+    });
+  }
+}
