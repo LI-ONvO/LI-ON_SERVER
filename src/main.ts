09fc0253daea6824d98a05db388ce3a2ad3f8b8d
tlgms <siheun9@gmail.com>
2026-08-19 19:05:37 +0900

    feat: 전역 입력 겁증 파이프라인 구축(#8)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationErrorException } from './common/exception/service.exception';
 import { ServiceExceptionToHttpExceptionFilter } from './common/exception/service.exception.to.http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      // 검증 실패도 ServiceException 으로 모아 응답 포맷을 통성일함.
+      exceptionFactory: (errors) =>
+        ValidationErrorException(
+          errors
+            .map((error) => Object.values(error.constraints ?? {}).join(', '))
+            .join(' / ') || undefined,
+        ),
+    }),
+  );
+
   app.useGlobalFilters(new ServiceExceptionToHttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
