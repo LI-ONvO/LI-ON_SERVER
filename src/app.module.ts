7fb756d018cc1fc1d1e115f4e0d7d142d92fea99
tlgms <siheun9@gmail.com>
2026-08-19 19:04:56 +0900

    feat: AuthModule 설정과 기존 MailModule의 책임 분리로 삭제(#8)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { RedisModule } from './common/redis/redis.module';
-import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     RedisModule,
-    MailModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
