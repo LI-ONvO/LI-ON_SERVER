cac3f1a3fb1c0d397433f50a420211c3d4c01aac
tlgms <siheun9@gmail.com>
2026-09-22 15:27:40 +0900

    feat: DeviceTokenService 모듈 등록(#38)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { DeviceTokenService } from './device-token.service';
 import { OnboardingService } from './onboarding.service';
 import { UserController } from './user.controller';
 import { UserProfileService } from './user.profile.service';
@@ -7,6 +8,6 @@ import { UserProfileService } from './user.profile.service';
 @Module({
   imports: [AuthModule],
   controllers: [UserController],
-  providers: [UserProfileService, OnboardingService],
+  providers: [UserProfileService, OnboardingService, DeviceTokenService],
 })
 export class UserModule {}
